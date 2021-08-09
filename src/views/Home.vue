97eac6d2ed11ed6c61598092cf0c7a304dfa6bb1
Peter Chae <htchae@gmail.com>
2021-08-10 06:21:47 +0900

    배포테스트1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,9 @@
 <template>
-  <hello-world />
+ <div>배포테스트1</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
     name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
